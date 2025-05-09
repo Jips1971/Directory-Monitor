60beee1b59e9b0d164e110f3fbcb727dd61347d0
James Blonden <james@blonden.co.uk>
2025-05-09 10:27:14 +0100

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,557 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        components = New ComponentModel.Container()
+        Timer1 = New Timer(components)
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        Label12 = New Label()
+        Label13 = New Label()
+        Label14 = New Label()
+        Label15 = New Label()
+        Label16 = New Label()
+        Label17 = New Label()
+        Label18 = New Label()
+        Label19 = New Label()
+        Label20 = New Label()
+        Label21 = New Label()
+        Label22 = New Label()
+        Label23 = New Label()
+        Label24 = New Label()
+        Label25 = New Label()
+        Label26 = New Label()
+        Label27 = New Label()
+        Label28 = New Label()
+        Label29 = New Label()
+        Label30 = New Label()
+        Label31 = New Label()
+        Label32 = New Label()
+        Label33 = New Label()
+        Label34 = New Label()
+        Label35 = New Label()
+        Label36 = New Label()
+        Label37 = New Label()
+        Label38 = New Label()
+        Label39 = New Label()
+        Label40 = New Label()
+        Label41 = New Label()
+        SuspendLayout()
+        ' 
+        ' Timer1
+        ' 
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(88, 86)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(54, 15)
+        Label1.TabIndex = 0
+        Label1.Text = "Beringen"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(88, 101)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(70, 15)
+        Label2.TabIndex = 1
+        Label2.Text = "Burghausen"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(88, 131)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(45, 15)
+        Label3.TabIndex = 3
+        Label3.Text = "Porvoo"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(88, 116)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(33, 15)
+        Label4.TabIndex = 2
+        Label4.Text = "Kallo"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Location = New Point(88, 161)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(77, 15)
+        Label5.TabIndex = 5
+        Label5.Text = "Stenungsund"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Location = New Point(88, 146)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(64, 15)
+        Label6.TabIndex = 4
+        Label6.Text = "Schwechat"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Location = New Point(254, 161)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(77, 15)
+        Label7.TabIndex = 11
+        Label7.Text = "Stenungsund"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Location = New Point(254, 146)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(64, 15)
+        Label8.TabIndex = 10
+        Label8.Text = "Schwechat"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Location = New Point(254, 131)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(45, 15)
+        Label9.TabIndex = 9
+        Label9.Text = "Porvoo"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Location = New Point(254, 116)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(33, 15)
+        Label10.TabIndex = 8
+        Label10.Text = "Kallo"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Location = New Point(254, 101)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(70, 15)
+        Label11.TabIndex = 7
+        Label11.Text = "Burghausen"
+        ' 
+        ' Label12
+        ' 
+        Label12.AutoSize = True
+        Label12.Location = New Point(254, 86)
+        Label12.Name = "Label12"
+        Label12.Size = New Size(54, 15)
+        Label12.TabIndex = 6
+        Label12.Text = "Beringen"
+        ' 
+        ' Label13
+        ' 
+        Label13.AutoSize = True
+        Label13.Location = New Point(430, 161)
+        Label13.Name = "Label13"
+        Label13.Size = New Size(77, 15)
+        Label13.TabIndex = 17
+        Label13.Text = "Stenungsund"
+        ' 
+        ' Label14
+        ' 
+        Label14.AutoSize = True
+        Label14.Location = New Point(430, 146)
+        Label14.Name = "Label14"
+        Label14.Size = New Size(64, 15)
+        Label14.TabIndex = 16
+        Label14.Text = "Schwechat"
+        ' 
+        ' Label15
+        ' 
+        Label15.AutoSize = True
+        Label15.Location = New Point(430, 131)
+        Label15.Name = "Label15"
+        Label15.Size = New Size(45, 15)
+        Label15.TabIndex = 15
+        Label15.Text = "Porvoo"
+        ' 
+        ' Label16
+        ' 
+        Label16.AutoSize = True
+        Label16.Location = New Point(430, 116)
+        Label16.Name = "Label16"
+        Label16.Size = New Size(33, 15)
+        Label16.TabIndex = 14
+        Label16.Text = "Kallo"
+        ' 
+        ' Label17
+        ' 
+        Label17.AutoSize = True
+        Label17.Location = New Point(430, 101)
+        Label17.Name = "Label17"
+        Label17.Size = New Size(70, 15)
+        Label17.TabIndex = 13
+        Label17.Text = "Burghausen"
+        ' 
+        ' Label18
+        ' 
+        Label18.AutoSize = True
+        Label18.Location = New Point(430, 86)
+        Label18.Name = "Label18"
+        Label18.Size = New Size(54, 15)
+        Label18.TabIndex = 12
+        Label18.Text = "Beringen"
+        ' 
+        ' Label19
+        ' 
+        Label19.AutoSize = True
+        Label19.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label19.Location = New Point(35, 86)
+        Label19.Name = "Label19"
+        Label19.Size = New Size(50, 15)
+        Label19.TabIndex = 18
+        Label19.Text = "Label19"
+        ' 
+        ' Label20
+        ' 
+        Label20.AutoSize = True
+        Label20.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label20.Location = New Point(35, 101)
+        Label20.Name = "Label20"
+        Label20.Size = New Size(50, 15)
+        Label20.TabIndex = 19
+        Label20.Text = "Label20"
+        ' 
+        ' Label21
+        ' 
+        Label21.AutoSize = True
+        Label21.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label21.Location = New Point(35, 116)
+        Label21.Name = "Label21"
+        Label21.Size = New Size(50, 15)
+        Label21.TabIndex = 20
+        Label21.Text = "Label21"
+        ' 
+        ' Label22
+        ' 
+        Label22.AutoSize = True
+        Label22.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label22.Location = New Point(35, 131)
+        Label22.Name = "Label22"
+        Label22.Size = New Size(50, 15)
+        Label22.TabIndex = 21
+        Label22.Text = "Label22"
+        ' 
+        ' Label23
+        ' 
+        Label23.AutoSize = True
+        Label23.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label23.Location = New Point(35, 146)
+        Label23.Name = "Label23"
+        Label23.Size = New Size(50, 15)
+        Label23.TabIndex = 22
+        Label23.Text = "Label23"
+        ' 
+        ' Label24
+        ' 
+        Label24.AutoSize = True
+        Label24.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label24.Location = New Point(35, 161)
+        Label24.Name = "Label24"
+        Label24.Size = New Size(50, 15)
+        Label24.TabIndex = 23
+        Label24.Text = "Label24"
+        ' 
+        ' Label25
+        ' 
+        Label25.AutoSize = True
+        Label25.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label25.Location = New Point(201, 86)
+        Label25.Name = "Label25"
+        Label25.Size = New Size(50, 15)
+        Label25.TabIndex = 29
+        Label25.Text = "Label25"
+        ' 
+        ' Label26
+        ' 
+        Label26.AutoSize = True
+        Label26.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label26.Location = New Point(201, 101)
+        Label26.Name = "Label26"
+        Label26.Size = New Size(50, 15)
+        Label26.TabIndex = 28
+        Label26.Text = "Label26"
+        ' 
+        ' Label27
+        ' 
+        Label27.AutoSize = True
+        Label27.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label27.Location = New Point(201, 116)
+        Label27.Name = "Label27"
+        Label27.Size = New Size(50, 15)
+        Label27.TabIndex = 27
+        Label27.Text = "Label27"
+        ' 
+        ' Label28
+        ' 
+        Label28.AutoSize = True
+        Label28.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label28.Location = New Point(201, 131)
+        Label28.Name = "Label28"
+        Label28.Size = New Size(50, 15)
+        Label28.TabIndex = 26
+        Label28.Text = "Label28"
+        ' 
+        ' Label29
+        ' 
+        Label29.AutoSize = True
+        Label29.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label29.Location = New Point(201, 146)
+        Label29.Name = "Label29"
+        Label29.Size = New Size(50, 15)
+        Label29.TabIndex = 25
+        Label29.Text = "Label29"
+        ' 
+        ' Label30
+        ' 
+        Label30.AutoSize = True
+        Label30.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label30.Location = New Point(201, 161)
+        Label30.Name = "Label30"
+        Label30.Size = New Size(50, 15)
+        Label30.TabIndex = 24
+        Label30.Text = "Label30"
+        ' 
+        ' Label31
+        ' 
+        Label31.AutoSize = True
+        Label31.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label31.Location = New Point(377, 86)
+        Label31.Name = "Label31"
+        Label31.Size = New Size(50, 15)
+        Label31.TabIndex = 35
+        Label31.Text = "Label31"
+        ' 
+        ' Label32
+        ' 
+        Label32.AutoSize = True
+        Label32.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label32.Location = New Point(377, 101)
+        Label32.Name = "Label32"
+        Label32.Size = New Size(50, 15)
+        Label32.TabIndex = 34
+        Label32.Text = "Label32"
+        ' 
+        ' Label33
+        ' 
+        Label33.AutoSize = True
+        Label33.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label33.Location = New Point(377, 116)
+        Label33.Name = "Label33"
+        Label33.Size = New Size(50, 15)
+        Label33.TabIndex = 33
+        Label33.Text = "Label33"
+        ' 
+        ' Label34
+        ' 
+        Label34.AutoSize = True
+        Label34.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label34.Location = New Point(377, 131)
+        Label34.Name = "Label34"
+        Label34.Size = New Size(50, 15)
+        Label34.TabIndex = 32
+        Label34.Text = "Label34"
+        ' 
+        ' Label35
+        ' 
+        Label35.AutoSize = True
+        Label35.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label35.Location = New Point(377, 146)
+        Label35.Name = "Label35"
+        Label35.Size = New Size(50, 15)
+        Label35.TabIndex = 31
+        Label35.Text = "Label35"
+        ' 
+        ' Label36
+        ' 
+        Label36.AutoSize = True
+        Label36.Font = New Font("Segoe UI", 9F, FontStyle.Bold)
+        Label36.Location = New Point(377, 161)
+        Label36.Name = "Label36"
+        Label36.Size = New Size(50, 15)
+        Label36.TabIndex = 30
+        Label36.Text = "Label36"
+        ' 
+        ' Label37
+        ' 
+        Label37.AutoSize = True
+        Label37.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
+        Label37.Location = New Point(35, 40)
+        Label37.Name = "Label37"
+        Label37.Size = New Size(111, 20)
+        Label37.TabIndex = 36
+        Label37.Text = "Prod - GS0758"
+        ' 
+        ' Label38
+        ' 
+        Label38.AutoSize = True
+        Label38.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
+        Label38.Location = New Point(201, 40)
+        Label38.Name = "Label38"
+        Label38.Size = New Size(107, 20)
+        Label38.TabIndex = 37
+        Label38.Text = "Test - GS0757"
+        ' 
+        ' Label39
+        ' 
+        Label39.AutoSize = True
+        Label39.Font = New Font("Segoe UI", 11F, FontStyle.Bold)
+        Label39.Location = New Point(377, 40)
+        Label39.Name = "Label39"
+        Label39.Size = New Size(105, 20)
+        Label39.TabIndex = 38
+        Label39.Text = "Dev - GS0756"
+        ' 
+        ' Label40
+        ' 
+        Label40.AutoSize = True
+        Label40.Location = New Point(370, 193)
+        Label40.Name = "Label40"
+        Label40.Size = New Size(64, 15)
+        Label40.TabIndex = 39
+        Label40.Text = "Version No"
+        ' 
+        ' Label41
+        ' 
+        Label41.AutoSize = True
+        Label41.Location = New Point(433, 193)
+        Label41.Name = "Label41"
+        Label41.Size = New Size(47, 15)
+        Label41.TabIndex = 40
+        Label41.Text = "Label41"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(527, 213)
+        Controls.Add(Label41)
+        Controls.Add(Label40)
+        Controls.Add(Label39)
+        Controls.Add(Label38)
+        Controls.Add(Label37)
+        Controls.Add(Label31)
+        Controls.Add(Label32)
+        Controls.Add(Label33)
+        Controls.Add(Label34)
+        Controls.Add(Label35)
+        Controls.Add(Label36)
+        Controls.Add(Label25)
+        Controls.Add(Label26)
+        Controls.Add(Label27)
+        Controls.Add(Label28)
+        Controls.Add(Label29)
+        Controls.Add(Label30)
+        Controls.Add(Label24)
+        Controls.Add(Label23)
+        Controls.Add(Label22)
+        Controls.Add(Label21)
+        Controls.Add(Label20)
+        Controls.Add(Label19)
+        Controls.Add(Label13)
+        Controls.Add(Label14)
+        Controls.Add(Label15)
+        Controls.Add(Label16)
+        Controls.Add(Label17)
+        Controls.Add(Label18)
+        Controls.Add(Label7)
+        Controls.Add(Label8)
+        Controls.Add(Label9)
+        Controls.Add(Label10)
+        Controls.Add(Label11)
+        Controls.Add(Label12)
+        Controls.Add(Label5)
+        Controls.Add(Label6)
+        Controls.Add(Label3)
+        Controls.Add(Label4)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Directory Monitor"
+        ResumeLayout(False)
+        PerformLayout()
+
+    End Sub
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+    Friend WithEvents Label12 As Label
+    Friend WithEvents Label13 As Label
+    Friend WithEvents Label14 As Label
+    Friend WithEvents Label15 As Label
+    Friend WithEvents Label16 As Label
+    Friend WithEvents Label17 As Label
+    Friend WithEvents Label18 As Label
+    Friend WithEvents Label19 As Label
+    Friend WithEvents Label20 As Label
+    Friend WithEvents Label21 As Label
+    Friend WithEvents Label22 As Label
+    Friend WithEvents Label23 As Label
+    Friend WithEvents Label24 As Label
+    Friend WithEvents Label25 As Label
+    Friend WithEvents Label26 As Label
+    Friend WithEvents Label27 As Label
+    Friend WithEvents Label28 As Label
+    Friend WithEvents Label29 As Label
+    Friend WithEvents Label30 As Label
+    Friend WithEvents Label31 As Label
+    Friend WithEvents Label32 As Label
+    Friend WithEvents Label33 As Label
+    Friend WithEvents Label34 As Label
+    Friend WithEvents Label35 As Label
+    Friend WithEvents Label36 As Label
+    Friend WithEvents Label37 As Label
+    Friend WithEvents Label38 As Label
+    Friend WithEvents Label39 As Label
+    Friend WithEvents Label40 As Label
+    Friend WithEvents Label41 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,78 @@
+﻿Imports System.IO
+Imports System.Reflection.Metadata.Ecma335
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Initialize all labels
+
+        'Version No set Here
+        Label41.Text = "v1.4"
+
+        Dim labels As Label() = {Label19, Label20, Label21, Label22, Label23, Label24,
+                         Label25, Label26, Label27, Label28, Label29, Label30,
+                         Label31, Label32, Label33, Label34, Label35, Label36}
+
+        For Each lbl As Label In labels
+            lbl.Text = ""
+        Next
+
+
+        Timer1.Interval = 1000
+        Timer1.Start()
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        ' Define the directory paths for Prod, Test, and Dev
+        Dim basePaths As String() = {
+            "\\GS0758\d$\RAPDATA\SAPtoRAP",
+            "\\GS0757\d$\RAPDATA\SAPtoRAP",
+            "\\GS0756\d$\RAPDATA\SAPtoRAP"
+        }
+
+        ' Define locations for each set of directories
+        Dim locations As String() = {
+            "Beringen", "Burghausen", "Kallo", "Porvoo", "Schwechat", "Stenungsund"
+        }
+
+        ' Define a start index for labels (starting from Label19)
+        Dim labelIndex As Integer = 19
+
+        ' Iterate through each base path (Prod, Test, Dev)
+        For Each basePath As String In basePaths
+            ' Iterate through each location
+            For Each location As String In locations
+                ' Construct the full directory path
+                Dim fullPath As String = Path.Combine(basePath, location)
+
+                ' Check if the directory exists and update the corresponding label
+                If Directory.Exists(fullPath) Then
+                    Me.Controls("Label" & labelIndex).ForeColor = Color.Green
+
+                    Me.Controls("Label" & labelIndex).Text = Directory.GetFiles(fullPath, "*.*").Count.ToString()
+                    If Directory.GetFiles(fullPath, "*.*").Count.ToString() > 0 Then
+                        Me.Controls("Label" & labelIndex).ForeColor = Color.Red
+                        'Flash Me
+                        Me.Controls("Label" & labelIndex).Visible = Not Me.Controls("Label" & labelIndex).Visible
+                    Else
+                        Me.Controls("Label" & labelIndex).ForeColor = Color.Green
+                        Me.Controls("Label" & labelIndex).Visible = True
+                    End If
+
+
+
+
+                Else
+                    Me.Controls("Label" & labelIndex).ForeColor = Color.Red
+                    Me.Controls("Label" & labelIndex).Text = "N/A"
+                End If
+
+                labelIndex += 1
+            Next
+        Next
+    End Sub
+
+
+
+
+
+End Class
